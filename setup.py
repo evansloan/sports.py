db926919612897c1dcbf4915503f7ce29f2a9bc4
Evan Sloan <evansloan082@gmail.com>
2017-06-01 06:30:34 -0400

    setup files for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='sports.py',
+    packages=['sports_py'],
+    version='1.0.',
+    description='A simple Python package to gather live sports scores',
+    author='Evan Sloan',
+    author_email='evansloan082@gmail.com',
+    url='https://github.com/evansloan082/sports.py',
+    download_url='https://github.com/evansloan082/sports.py/archive/1.0.tar.gz',
+    keywords=['sports', 'scores', 'live scores', 'hockey', 'basketball', 'baseball', 'football'],
+    classifiers=[],
+)
