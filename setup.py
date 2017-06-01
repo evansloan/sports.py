0d0a7f440c50652bdbe8c2e9d460425b82031f73
Evan Sloan <evansloan082@gmail.com>
2017-06-01 06:41:55 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='sports.py',
     packages=['sports_py'],
-    version='1.0.',
+    version='1.0.2',
     description='A simple Python package to gather live sports scores',
     author='Evan Sloan',
     author_email='evansloan082@gmail.com',
     url='https://github.com/evansloan082/sports.py',
-    download_url='https://github.com/evansloan082/sports.py/archive/1.0.tar.gz',
+    download_url='https://github.com/evansloan082/sports.py/archive/1.0.2.tar.gz',
     keywords=['sports', 'scores', 'live scores', 'hockey', 'basketball', 'baseball', 'football'],
     classifiers=[],
 )
