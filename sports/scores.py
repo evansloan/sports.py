c9e40bff7c6e8c4f6012ebbde30e67a3f874ca46
Evan Sloan <evansloan082@gmail.com>
2018-07-19 21:05:20 -0400

    Refactor scores.py
    Remove match info parsing from get_sport_scores() function and into separate function _parse_match_info()
    Rename get_match() to match()
    Rename get_all_matches() to all_matches()
    Remove to_json() method in Match object

--- sports/scores.py
+++ sports/scores.py
@@ -1,4 +1,3 @@
-import json
 import re
 import xml.etree.ElementTree as ET
 from datetime import datetime
@@ -15,6 +14,7 @@ class Match:
         self.home_score = score[0]
         self.away_score = score[1]
         self.match_date = datetime.strptime(match_info['match_date'], '%a, %d %b %Y %H:%M:%S %Z')
+        self.raw = match_info
 
         for key, value in match_info.items():
             if key not in ('match_score', 'match_date'):
@@ -28,97 +28,85 @@ class Match:
         return '{} {}-{} {}'.format(self.home_team, self.home_score,
                                     self.away_score, self.away_team)
 
-    def to_json(self):
-        """
-        Convert match object into JSON
-        :return: JSON data containing match info
-        """
-        data = {
-            'match': {
-                'sport': self.sport,
-                'home_team': self.home_team,
-                'away_team': self.away_team,
-                'home_score': self.home_score,
-                'away_score': self.away_score,
-                'match_time': self.match_time,
-                'match_link': self.match_link
-            }
-        }
-        return json.dumps(data)
-
 
 def _load_xml(sport):
     """
     Parse XML file containing match details using ElementTree
+
     :param sport: sport being played
     :type sport: string
-    :return: ElementTree object containing data from XML file
+    :return: ElementTree instance containing data from XML file
+    :rtyp: ElementTree instance
     """
     try:
         url = 'http://www.scorespro.com/rss2/live-{}.xml'.format(sport)
         r = requests.get(url)
-        return ET.fromstring(r.content)
+        return ET.fromstring(r.content).find('channel').findall('item')
     except ET.ParseError:
         raise errors.SportError(sport)
+
+
+def _parse_match_info(match, soccer=False):
+    """
+    Parse string containing info of a specific match
+
+    :param match: Match data
+    :type match: string
+    :param soccer: Set to true if match contains soccer data, defaults to False
+    :type soccer: bool, optional
+    :return: Dictionary containing match information
+    :rtype: dict
+    """
+
+    match_info = {}
+
+    i_open = match.index('(')
+    i_close = match.index(')')
+    match_info['league'] = match[i_open + 1:i_close].strip()
+
+    match = match[i_close + 1:]
+    i_vs = match.index('vs')
+    i_colon = match.index(':')
+    match_info['home_team'] = match[0:i_vs].replace('#', ' ').strip()
+    match_info['away_team'] = match[i_vs + 2:i_colon].replace('#', ' ').strip()
+    match = match[i_colon:]
+
+    if soccer:
+        i_hyph = match.index('-')
+        match_info['match_score'] = match[1:i_hyph + 2].strip()
+        match = match[i_hyph + 1:]
+        i_hyph = match.index('-')
+        match_info['match_time'] = match[i_hyph + 1:].strip()
+    else:
+        match_info['match_score'] = match[1:].strip()
+        match_info['match_time'] = match.find('description').text.strip()
+
+    match_info['match_date'] = match.find('pubDate').text.strip()
+    match_info['match_link'] = match.find('guid').text.strip()
+
+    return match_info
 
 
 def get_sport_scores(sport):
     """
     Get live scores for all matches in a particular sport
+
     :param sport: the sport being played
     :type sport: string
     :return: List containing Match objects
+    :rtype: list
     """
     sport = sport.lower()
-    root = _load_xml(sport)
+    data = _load_xml(sport)
 
-    items = []
-    for child in root:
-        for c in child:
-            if c.tag == 'item':
-                items.append(c)
-
-    match_info = {}
     matches = []
-    for item in items:
-        for child in item:
-            if sport == constants.SOCCER:
-                if child.tag == 'description':
-                    title = child.text
-                    i_open = title.index('(')
-                    i_close = title.index(')')
-                    match_info['league'] = title[i_open + 1:i_close].strip()
-                    title = title[i_close + 1:]
-                    i_vs = title.index('vs')
-                    i_colon = title.index(':')
-                    match_info['home_team'] = title[0:i_vs].replace('#', ' ').strip()
-                    match_info['away_team'] = title[i_vs + 2:i_colon].replace('#', ' ').strip()
-                    title = title[i_colon:]
-                    i_hyph = title.index('-')
-                    match_info['match_score'] = title[1:i_hyph + 2].strip()
-                    title = title[i_hyph + 1:]
-                    i_hyph = title.index('-')
-                    match_info['match_time'] = title[i_hyph + 1:].strip()
-            else:
-                if child.tag == 'title':
-                    title = child.text
-                    i_open = title.index('(')
-                    i_close = title.index(')')
-                    match_info['league'] = title[i_open + 1:i_close].strip()
-                    title = title[i_close + 1:]
-                    i_vs = title.index('vs')
-                    i_colon = title.index(':')
-                    match_info['home_team'] = title[0:i_vs].replace('#', ' ').strip()
-                    match_info['away_team'] = title[i_vs + 2:i_colon].replace('#', ' ').strip()
-                    match_info['match_score'] = title[i_colon + 1:].strip()
-
-                if child.tag == 'description':
-                    match_info['match_time'] = child.text.strip()
-
-            if child.tag == 'pubDate':
-                match_info['match_date'] = child.text.strip()
-            if child.tag == 'guid':
-                match_info['match_link'] = child.text.strip()
+    for match in data:
+        if sport == constants.SOCCER:
+            desc = match.find('description').text
+            match_info = _parse_match_info(desc, soccer=True)
+        else:
+            desc = match.find('title').text
+            match_info = _parse_match_info(desc)
 
         matches.append(Match(sport, match_info))
 
@@ -128,13 +116,15 @@ def get_sport_scores(sport):
 def match(sport, team1, team2):
     """
     Get live scores for a single match
+
     :param sport: the sport being played
-    :param team1: first team participating in the match
-    :param team2: second team participating in the match
     :type sport: string
-    :type team1: string
+    :param team1: first team participating in the match
+    :ttype team1: string
+    :param team2: second team participating in the match
     :type team2: string
-    :return: Match object
+    :return: A specific match
+    :rtype: Match
     """
     sport = sport.lower()
     team1_pattern = re.compile(team1, re.I)
@@ -153,7 +143,9 @@ def all_matches():
     """
     Get a list of lists containing all live matches.
     Each sport is contained within its own list
+
     :return: List containing lists of match objects
+    :rtype: list
     """
     sports = ['baseball', 'basketball', 'hockey', 'football', 'rugby-union',
               'rugby-league', 'tennis', 'soccer', 'handball', 'volleyball']
