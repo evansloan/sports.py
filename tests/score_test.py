4beac62566300a137899372af79204f39fe7be2c
Evan Sloan <evansloan082@gmail.com>
2018-07-25 15:15:34 -0400

    add tests

--- tests/score_test.py
+++ tests/score_test.py
@@ -17,8 +17,18 @@ class TestScores(unittest.TestCase):
     match = sports.scores.Match(sports.HOCKEY, match_data)
     matches = sports.get_sport(sports.BASEBALL)
 
+    def test_xml(self):
+        try:
+            sports.scores._request_xml(sports.BASEBALL)
+        except sports.errors.SportError:
+            self.fail('XML request raised SportError')
+
+        self.assertRaises(sports.errors.SportError, sports.scores._request_xml, 'fake sport')
+
     def test_match(self):
         self.assertIsNotNone(self.match)
+        self.assertEqual(str(self.match), 'Pittsburgh Penguins 2-0 Nashville Predators')
+        self.assertEqual(repr(self.match), 'Pittsburgh Penguins 2-0 Nashville Predators')
 
     def test_teams(self):
         self.assertEqual(self.match.home_team, 'Pittsburgh Penguins')
