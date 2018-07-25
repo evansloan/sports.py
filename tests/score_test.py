b41ff092274fd5a9599da1912a83eb313a75917b
Evan Sloan <evansloan082@gmail.com>
2018-07-25 15:33:05 -0400

    updates scores.py tests

--- tests/score_test.py
+++ tests/score_test.py
@@ -4,7 +4,7 @@ import sports
 
 
 class TestScores(unittest.TestCase):
-    match_data = {
+    hockey_data = {
         'league': 'NHL',
         'home_team': 'Pittsburgh Penguins',
         'away_team': 'Nashville Predators',
@@ -14,8 +14,7 @@ class TestScores(unittest.TestCase):
         'match_link': 'test',
     }
 
-    match = sports.scores.Match(sports.HOCKEY, match_data)
-    matches = sports.get_sport(sports.BASEBALL)
+    hockey_match = sports.scores.Match(sports.HOCKEY, hockey_data)
 
     def test_xml(self):
         try:
@@ -26,23 +25,26 @@ class TestScores(unittest.TestCase):
         self.assertRaises(sports.errors.SportError, sports.scores._request_xml, 'fake sport')
 
     def test_match(self):
-        self.assertIsNotNone(self.match)
-        self.assertEqual(str(self.match), 'Pittsburgh Penguins 2-0 Nashville Predators')
-        self.assertEqual(repr(self.match), 'Pittsburgh Penguins 2-0 Nashville Predators')
+        self.assertIsNotNone(self.hockey_match)
+        self.assertEqual(str(self.hockey_match), 'Pittsburgh Penguins 2-0 Nashville Predators')
+        self.assertEqual(repr(self.hockey_match), 'Pittsburgh Penguins 2-0 Nashville Predators')
+
+        self.assertIsNotNone(sports.all_matches())
 
     def test_teams(self):
-        self.assertEqual(self.match.home_team, 'Pittsburgh Penguins')
-        self.assertEqual(self.match.away_team, 'Nashville Predators')
+        self.assertEqual(self.hockey_match.home_team, 'Pittsburgh Penguins')
+        self.assertEqual(self.hockey_match.away_team, 'Nashville Predators')
 
     def test_score(self):
-        self.assertEqual(self.match.home_score, 2)
-        self.assertEqual(self.match.away_score, 0)
+        self.assertEqual(self.hockey_match.home_score, 2)
+        self.assertEqual(self.hockey_match.away_score, 0)
 
     def test_date(self):
-        self.assertIsNotNone(self.match.match_date)
+        self.assertIsNotNone(self.hockey_match.match_date)
 
     def test_sport(self):
-        self.assertEqual(self.match.sport, sports.HOCKEY)
+        self.assertEqual(self.hockey_match.sport, sports.HOCKEY)
+        self.assertIsNone(sports.get_sport(sports.SOCCER))
 
 
 if __name__ == '__main__':
