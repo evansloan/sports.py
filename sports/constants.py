a232022b9bfa2e7f4289e95749e54fff9b168bca
Evan Sloan <evansloan082@gmail.com>
2018-09-08 09:02:55 -0400

    Add missing rugby constants

--- sports/constants.py
+++ sports/constants.py
@@ -1,10 +1,16 @@
 BASEBALL = 'baseball'
 BASKETBALL = 'basketball'
-CFB = 'cfb'
 CRICKET = 'cricket'
 FOOTBALL = 'football'
 HANDBALL = 'handball'
 HOCKEY = 'hockey'
+RUGBY_L = 'rugby-league'
+RUGBY_U = 'rugby-union'
 SOCCER = 'soccer'
 TENNIS = 'tennis'
 VOLLEYBALL = 'volleyball'
+
+SPORTS = [
+    BASEBALL, BASKETBALL, CRICKET, FOOTBALL, HANDBALL, HOCKEY, RUGBY_L,
+    RUGBY_U, SOCCER, TENNIS, VOLLEYBALL
+]
